061703761deeefd3ae93d4a783c5e4f6fa687496
Sivanujan Selvarajah <selvasiv@students.zhaw.ch>
2024-03-19 16:40:36 +0100

    .....

---
+++ Week_03/exercises/Docker_QRCode_Generator/my_script.py
@@ -0,0 +1,16 @@
+import time
+import sys
+from pystrich.qrcode import QRCodeEncoder
+
+# Creates QR-Code based on text input
+encoder = QRCodeEncoder("Docker ist cool!")
+encoder.save("./QRCode_test.png")
+
+# Prints ASCII-version of the QR-Code
+print(encoder.get_ascii())
+print('Container is running, use another VS Code terminal!')
+sys.stdout.flush()
+
+# Prevents the container from stopping
+while True:
+    time.sleep(1)
